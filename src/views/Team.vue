c1c957a9dffed0855ecb2da78c6df47fc099afc8
iRuxu <rx6@qq.com>
2021-04-19 16:08:10 +0800

    update macro

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -21,7 +21,7 @@
         <member list="2,15,1101,11,16904,7898" :duty="production" />
 
         <el-divider content-position="left">编辑/审核</el-divider>
-        <member list="44,904,2394,62,3217,2932,20273,49682,1941,49990" :duty="content" />
+        <member list="44,904,8418,10250,2394,62,3217,2932,20273,49682,1941,49990" :duty="content" />
 
         <el-divider content-position="left">贡献名单</el-divider>
         <member list="17,1972,1364,30,372" :duty="contributor" />
@@ -81,6 +81,8 @@ export default {
             content: {
                 44: "宏库",
                 904: "宏库",
+                8418: "宏库",
+                10250: "宏库",
                 7898:"百科",
                 2394:"百科",
                 3217:"百科",
