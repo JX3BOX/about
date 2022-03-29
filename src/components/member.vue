bb171b6a57b543775d992bc2d0b2ba9355e04b3b
iRuxu <rx6@qq.com>
2022-03-30 01:57:44 +0800

    sync common:

--- src/components/member.vue
+++ src/components/member.vue
@@ -1,14 +1,9 @@
 <template>
     <div class="m-about-member" v-if="list && list.length">
         <div class="u-list">
-            <a
-                class="u-member"
-                :href="item.user_id | authorLink"
-                v-for="(item, i) in list"
-                :key="i"
-            >
-                <img :src="item.teammate_info.user_avatar | showAvatar" />
-                <span>{{ item.teammate_info.display_name }}</span>
+            <a class="u-member" :href="item.user_id | authorLink" v-for="(item, i) in list" :key="i">
+                <img :src="getAvatar(item)" />
+                <span>{{ getUsername(item) }}</span>
                 <em>{{ item.remark }}</em>
             </a>
         </div>
@@ -16,66 +11,70 @@
 </template>
 
 <script>
-import { authorLink, showAvatar } from "@jx3box/jx3box-common/js/utils";
-export default {
-    name: "member",
-    props: ["list"],
-    data: function () {
-        return {
-            data: [],
-        };
-    },
-    computed: {},
-    filters: {
-        authorLink: function (uid) {
-            return authorLink(uid);
+    import { authorLink, showAvatar } from "@jx3box/jx3box-common/js/utils";
+    export default {
+        name: "member",
+        props: ["list"],
+        data: function() {
+            return {
+                data: [],
+            };
         },
-        showAvatar: function (url) {
-            return showAvatar(url);
+        methods: {
+            getAvatar: function(item) {
+                return showAvatar(item?.teammate_info?.user_avatar);
+            },
+            getUsername: function(item) {
+                return item?.teammate_info?.display_name;
+            },
         },
-    },
-    mounted: function () {},
-    components: {},
-};
+        filters: {
+            authorLink: function(uid) {
+                return authorLink(uid);
+            },
+        },
+        mounted: function() {},
+        components: {},
+    };
 </script>
 
 <style lang="less">
-.m-about-member {
-    .u-list {
-        .clearfix;
-    }
-    .u-member {
-        .db;
-        .fl;
-        .w(320px);
-        .mr(20px);
-        .mb(20px);
-        font-weight: 300;
-        color: @color;
-
-        img {
+    .m-about-member {
+        .u-list {
+            .clearfix;
+        }
+        .u-member {
+            .db;
             .fl;
+            .w(320px);
             .mr(20px);
-            .r(50%);
-            .size(68px);
-        }
+            .mb(20px);
+            font-weight: 300;
+            color: @color;
 
-        span {
-            .lh(42px);
-            .db;
-        }
+            img {
+                .fl;
+                .mr(20px);
+                .r(50%);
+                .size(68px);
+            }
 
-        em {
-            .lh(18px);
-            .db;
-            .fz(12px);
-        }
-
-        &:hover {
             span {
-                color: @primary;
+                .lh(42px);
+                .db;
+            }
+
+            em {
+                .lh(18px);
+                .db;
+                .fz(12px);
+            }
+
+            &:hover {
+                span {
+                    color: @primary;
+                }
             }
         }
     }
-}
 </style>
