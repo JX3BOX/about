de22045da615fa5bb65679408844eb50d6573c03
zixuan1986 <litthy@qq.com>
2022-05-20 16:10:25 +0800

    fix:团队加介绍

--- src/components/member.vue
+++ src/components/member.vue
@@ -1,78 +1,106 @@
 <template>
     <div class="m-about-member" v-if="list && list.length">
-        <div class="u-list">
-            <a class="u-member" :href="item.user_id | authorLink" v-for="(item, i) in list" :key="i">
+        <div class="u-list" v-for="(item, i) in list" :key="i">
+            <a class="u-member" :href="authorLink(item.user_id)">
                 <img :src="getAvatar(item)" />
-                <span>{{ getUsername(item) }}</span>
-                <em>{{ item.remark }}</em>
+                <el-popover popper-class="m-about-popover" placement="right-start" trigger="hover" width="240">
+                    <div class="u-user">
+                        <el-divider content-position="left">{{ getUsername(item) }}</el-divider>
+                        <div class="u-tags" v-if="item.duty && item.duty.length">
+                            <el-tag class="u-tag" size="small" v-for="key in item.duty" :key="key"> {{showTag(key)}} </el-tag>
+                        </div>
+                        <span v-html="item.desc" v-if="item.desc"></span> <span v-else>暂无介绍</span>
+                    </div>
+                    <div slot="reference" class="u-info">
+                        <span>{{ getUsername(item) }}</span>
+                        <em>{{ item.remark }}</em>
+                    </div>
+                </el-popover>
             </a>
         </div>
     </div>
 </template>
 
 <script>
-    import { authorLink, showAvatar } from "@jx3box/jx3box-common/js/utils";
-    export default {
-        name: "member",
-        props: ["list"],
-        data: function() {
-            return {
-                data: [],
-            };
+import { authorLink, showAvatar } from '@jx3box/jx3box-common/js/utils'
+import { showDate } from '@jx3box/jx3box-common/js/moment'
+import usergroups from '@/assets/data/usergroups.json'
+export default {
+    name: 'member',
+    props: ['list'],
+    data: function () {
+        return {
+            data: [],
+            tag: usergroups,
+        }
+    },
+    methods: {
+        getAvatar: function (item) {
+            return showAvatar(item?.teammate_info?.user_avatar, 'm')
         },
-        methods: {
-            getAvatar: function(item) {
-                return showAvatar(item?.teammate_info?.user_avatar,'m');
-            },
-            getUsername: function(item) {
-                return item?.teammate_info?.display_name;
-            },
+        getUsername: function (item) {
+            return item?.teammate_info?.display_name
         },
-        filters: {
-            authorLink: function(uid) {
-                return authorLink(uid);
-            },
+        authorLink,
+        showDate,
+        showTag(key) {
+            return this.tag[key] || key
         },
-        mounted: function() {},
-        components: {},
-    };
+    },
+
+    mounted: function () {},
+    components: {},
+}
 </script>
 
 <style lang="less">
     .m-about-member {
+        .flex;
+        flex-wrap: wrap;
         .u-list {
-            .clearfix;
+            .mb(40px);
+            width: 30%;
+            .u-member {
+                .flex;
+                font-weight: 300;
+                img {
+                    .fl;
+                    .mr(20px);
+                    .r(50%);
+                    .size(68px);
+                }
+                .u-info {
+                    .flex;
+                    flex-direction: column;
+                    color: @color;
+                    span {
+                        .lh(42px);
+                        .db;
+                        &:hover {
+                            color: @primary;
+                        }
+                    }
+
+                    em {
+                        .lh(18px);
+                        .db;
+                        .fz(12px);
+                    }
+                }
+            }
         }
-        .u-member {
-            .db;
-            .fl;
-            .w(320px);
-            .mr(20px);
-            .mb(20px);
-            font-weight: 300;
-            color: @color;
+    }
+    .m-about-popover {
+        .u-user {
+            .flex;
+            cursor: default;
+            flex-direction: column;
 
-            img {
-                .fl;
-                .mr(20px);
-                .r(50%);
-                .size(68px);
-            }
-
-            span {
-                .lh(42px);
-                .db;
-            }
-
-            em {
-                .lh(18px);
-                .db;
-                .fz(12px);
-            }
-
-            &:hover {
-                span {
-                    color: @primary;
+            .u-tags {
+                border-bottom: 1px solid #dcdfe6;
+                .pb(20px);
+                .u-tag {
+                    margin: 5px;
                 }
             }
         }
